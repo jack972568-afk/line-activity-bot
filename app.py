cc606f7d0ce19bf79775beb97eed2e34eea09d52
jack972568-afk <jack972568@gmail.com>
2026-07-03 21:41:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import google.generativeai as genai
 
 app = Flask(__name__)
